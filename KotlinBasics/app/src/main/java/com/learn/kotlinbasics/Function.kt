b7065824059549a6de1b145f0509d0474b924015
NAUFALZULHM <naufalmaulanasans@gmail.com>
2025-03-12 22:02:36 +0700

    make function to make a coffee

--- KotlinBasics/app/src/main/java/com/learn/kotlinbasics/Function.kt
+++ KotlinBasics/app/src/main/java/com/learn/kotlinbasics/Function.kt
@@ -3,25 +3,39 @@ package com.learn.kotlinbasics
 // Call Function
 fun main(){
 
+    add(1,4)
     // Call Function
+    println("Who is coffee for ?")
+    val name = readln()
+    println("How many pieces of sugar do you want ?")
+    val sugarCount = readln().toInt()
 
-    makeCoffee(1,"Maulana")
+    makeCoffee(sugarCount,name)
 
-    makeCoffee(2,"Jimmy")
-
-    makeCoffee(30,"Helmi")
-
-    makeCoffee(0,"Zul")
-    makeCoffee(-1,"Naufal")
+//    println("Do you want make a coffee again ? Please select Yes or No")
+//    val makeDecision = readln().trim().lowercase()
+//    makeAnotherCoffee(makeDecision)
 
 }
 
 
+fun add(num1: Int, num2: Int): Int {
+    return num1 + num2
+}
+
+
+
 
 // Define Function
+//fun makeAnotherCoffee(makeDecision: String) {
+//    if (makeDecision == "yes"){
+//        return makeCoffee()
+//    }
+//}
 
-fun makeCoffee(sugarCount : Int , name : String){
-    if(sugarCount == 2){
+
+fun makeCoffee(sugarCount: Int, name: String){
+    if(sugarCount > 1){
         println("Coffee with $sugarCount spoons of sugar for $name")
     }
     else if (sugarCount == 0){
@@ -31,7 +45,13 @@ fun makeCoffee(sugarCount : Int , name : String){
         println("Coffee with $sugarCount spoon of sugar for $name")
     }
     else{
-        println("Coffee with $sugarCount spoons of sugar for $name")
+        println("No Coffee for anyone")
     }
 
 }
+
+
+
+
+
+
